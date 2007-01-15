7f2e99c1f7f7747237a447a56ecb11dcc717b019
ian <ian@adcf324d-f026-0410-96c8-8dcf6f387dfc>
2007-01-15 12:13:58 +0000

    Adding resources event and user
    git-svn-id: http://svn.ardes.com/cadre/trunk@15 adcf324d-f026-0410-96c8-8dcf6f387dfc

---
+++ db/migrate/001_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration
+  def self.up
+    create_table :events do |t|
+      t.column :type, :string
+      t.column :ip_address, :string
+      t.column :created_at, :datetime
+    end
+  end
+
+  def self.down
+    drop_table :events
+  end
+end
