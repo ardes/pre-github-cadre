776192c5835e00a3a552fd84a5fe1c04d20d4e6d
ian <ian@adcf324d-f026-0410-96c8-8dcf6f387dfc>
2007-01-18 17:56:00 +0000

    spec seems to run in development unless explicitly told not to
    git-svn-id: http://svn.ardes.com/cadre/trunk@21 adcf324d-f026-0410-96c8-8dcf6f387dfc

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+ENV["RAILS_ENV"] = "test"
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
