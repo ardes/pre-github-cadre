9a3f33da26721f7541b2dffb7e70b8a3950c9137
ian <ian@adcf324d-f026-0410-96c8-8dcf6f387dfc>
2007-01-22 11:53:42 +0000

    Completed Event::GenerateKey spec
    git-svn-id: http://svn.ardes.com/cadre/trunk@33 adcf324d-f026-0410-96c8-8dcf6f387dfc

--- spec/models/event_generate_key_spec.rb
+++ spec/models/event_generate_key_spec.rb
@@ -20,3 +20,52 @@ context "An Event class with GenerateKey mixin" do
     lambda{ EventWithGenerateKey.key_algorithm = 'foo' }.should_raise ArgumentError
   end
 end
+
+context "A new Event with GenerateKey mixin" do
+  fixtures :event_properties
+  
+  setup do
+    @event = EventWithGenerateKey.new
+  end
+  
+  specify "should generate a key and key_hash on create" do
+    @event.save
+    @event.key.should_be_kind_of(String)
+    @event.key_hash.should_be_kind_of(String)
+  end
+end
+
+context "An existing Event with GenerateKey mixin" do
+  fixtures :event_properties
+  
+  setup do
+    @event = EventWithGenerateKey.create
+    @key = @event.key
+    @event = EventWithGenerateKey.find(@event.id) # reload from db
+  end
+  
+  specify "should not have a key" do
+    @event.key.should == nil
+  end
+
+  specify "should have a key_hash" do
+    @event.key_hash.should_be_kind_of(String)
+  end
+  
+  specify "should match_key_hash with correct key" do
+    @event.should_match_key_hash(@key)
+  end
+
+  specify "should not match_key_hash with incorrect key" do
+    @event.should_not_match_key_hash(@key + 'foo')
+  end
+  
+  specify "should be findable with class find_by_id_and_key_hash" do
+    EventWithGenerateKey.find_by_id_and_key_hash(@event.id, @event.key_hash).should == @event
+  end
+  
+  specify "should be findable with class find_by_id_and_key" do
+    EventWithGenerateKey.find_by_id_and_key(@event.id, @key).should == @event
+  end
+end
+
