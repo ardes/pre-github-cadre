300335c19f64c2fee48d2408986ac7afdd734dc6
ian <ian@adcf324d-f026-0410-96c8-8dcf6f387dfc>
2007-01-22 12:12:22 +0000

    Added Signup Spec
    git-svn-id: http://svn.ardes.com/cadre/trunk@34 adcf324d-f026-0410-96c8-8dcf6f387dfc

---
+++ spec/models/signup_spec.rb
@@ -0,0 +1,40 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+context "The Signup class" do
+  specify "should have GenerateKey mixin" do
+    Signup.included_modules.should_include Event::GenerateKey
+  end
+end
+
+context "A new Signup" do
+  fixtures :users, :events, :event_properties
+
+  setup do
+    @signup = Signup.new 'email' => 'satan@hell.com', :password => 'kittens' # begin with a valild signup
+  end
+  
+  specify "should build :user on demand" do
+    @signup.user.should_be_kind_of User
+  end
+  
+  specify "should delegate :email, :email_confirmation, :display_name, :password and :password_confirmation (getter and setter) to :user" do
+    delegate_methods = [:email, :email_confirmation, :display_name, :password, :password_confirmation]
+    delegate_methods.each do |m|
+      @signup.send "#{m}=", 'foo'
+      @signup.user.send(m).should == 'foo'
+      @signup.send(m).should == 'foo'
+    end
+  end
+    
+  specify "should be invalid without a valid :user" do
+    @signup.user.email_confirmation = 'kitten@gmail.com'
+    @signup.user.should_not_be_valid
+    @signup.should_not_be_valid
+  end
+  
+  specify "should merge inavlid user errors into errors" do
+    @signup.attributes = {:password => 'gunk', :email => 'jkljkljkjkjkl', :email_confirmation => '8908989089080'}
+    @signup.should_not_be_valid
+    @signup.errors.full_messages.to_set.should == @signup.user.errors.full_messages.to_set << 'User is invalid'
+  end
+end
