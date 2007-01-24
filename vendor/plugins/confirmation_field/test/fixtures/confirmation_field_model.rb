21f133948cacb87b66ae4af922ebd2a80be971fe
ian <ian@adcf324d-f026-0410-96c8-8dcf6f387dfc>
2007-01-24 10:43:39 +0000

    Removing confirmation_field plugin
    git-svn-id: http://svn.ardes.com/cadre/trunk@49 adcf324d-f026-0410-96c8-8dcf6f387dfc

--- vendor/plugins/confirmation_field/test/fixtures/confirmation_field_model.rb
+++
@@ -1,4 +0,0 @@
-class ConfirmationFieldModel < ActiveRecord::Base# :nodoc:
-  validates_confirmation_of :email
-  validates_confirmation_of :password
-end
--- vendor/plugins/confirmation_field/test/schema.rb
+++
@@ -1,7 +0,0 @@
-ActiveRecord::Schema.define(:version => 0) do
-  create_table :confirmation_field_models, :force => true do |t|
-    t.column "name", :string
-    t.column "email", :string
-    t.column "password", :string
-  end
-end
