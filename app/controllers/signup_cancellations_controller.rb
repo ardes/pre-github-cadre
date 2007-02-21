830b136661131998693c206982bbb7047d744875
ian <ian@adcf324d-f026-0410-96c8-8dcf6f387dfc>
2007-02-21 17:05:45 +0000

    SignupCancellation takes params and uses them as resource params
    git-svn-id: http://svn.ardes.com/cadre/trunk@87 adcf324d-f026-0410-96c8-8dcf6f387dfc

--- app/controllers/signup_cancellations_controller.rb
+++ app/controllers/signup_cancellations_controller.rb
@@ -1,3 +1,8 @@
 class SignupCancellationsController < EventsController
   resources_controller_for :signup_cancellations
+  
+  def create
+    params[resource_name] = {:signup_key => params[:signup_key], :signup_id => params[:signup_id]}
+    super
+  end
 end
